20966ed65b434e15e9c016329905368907add77b
Franklin Ohaegbulam <46662771+frankiefab100@users.noreply.github.com>
2022-12-04 04:46:26 +0100

    refactor: update script

--- script.js
+++ script.js
@@ -2,31 +2,36 @@ const searchbar = document.querySelector(".search");
 const submitBtn = document.querySelector(".search-btn");
 const parentContainer = document.querySelector(".container");
 const photoWrapper = document.querySelector(".photo-wrapper");
-const photoElement = document.querySelector(".photo");
 
 submitBtn.addEventListener("click", () => {
-  getCondition(searchbar.value);
+  getPhoto(searchbar.value);
   searchbar.value = "";
 });
 
 window.addEventListener("keydown", (e) => {
   if (e.keyCode === 13) {
-    getCondition(searchbar.value);
+    getPhoto(searchbar.value);
     searchbar.value = "";
   }
 });
 
-function getCondition(keyword) {
+function getPhoto(keyword) {
   const apiKey = "10694009-840350dba3c047ed6f6b4f364";
-  let apiURL = `https://pixabay.com/api/?key=${apiKey}&q=${keyword}`;
+  let apiURL = `https://pixabay.com/api/?key=${apiKey}&q=${keyword}&image_type=photo&safesearch=true&per_page=3`;
 
-  fetch(apiURL)
+  fetch(apiURL, {
+    method: "GET",
+    headers: { accept: "application/json" },
+  })
     .then((response) => response.json())
     .then((data) => {
-      let imageURL = data.hits[0].pageURL;
-      let imageElement = document.createElement("img");
-      imageElement.setAttribute("src", `${imageURL}`);
-      photoWrapper.appendChild(imageElement);
+      let imageURL = data.hits;
+
+      imageURL.forEach((result) => {
+        let imageElement = document.createElement("img");
+        imageElement.setAttribute("src", `${result.webformatURL}`);
+        photoWrapper.appendChild(imageElement);
+      });
     })
     .catch((error) => alert(error.message));
 }
