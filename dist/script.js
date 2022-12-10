31795205c3364c250c220fc875039f0f82455d97
frankiefab100 <46662771+frankiefab100@users.noreply.github.com>
2022-12-10 15:12:33 +0100

    fix: resolve fetch request in getPhotos.js

--- dist/script.js
+++ dist/script.js
@@ -5,12 +5,14 @@ const photoWrapper = document.querySelector(".photo-wrapper");
 submitBtn.addEventListener("click", () => {
   getPhoto(searchbar.value);
   searchbar.value = "";
+  photoWrapper.innerHTML = "";
 });
 
 window.addEventListener("keydown", (e) => {
   if (e.keyCode === 13) {
     getPhoto(searchbar.value);
     searchbar.value = "";
+    photoWrapper.innerHTML = "";
   }
 });
 
@@ -18,53 +20,18 @@ async function getPhoto(keyword) {
   let apiURL = `/.netlify/functions/getPhotos?keyword=${keyword}`;
 
   try {
-    const response = await fetch(apiURL);
-
-    // if (response.ok) {
-    //   console.log(await response.json());
-    // }
-
+    const response = await fetch(apiURL, {
+      method: "GET",
+      headers: { accept: "application/json" },
+    });
     const data = await response.json();
 
-    // let images = [];
-    // if (!!data?.length) {
-    //   images = data.map((image) => image.toString());
-    // }
-
-    // let img = data.forEach(async (image) => {
-    //   console.log(await image.buffer());
-    //   await image.buffer();
-    // });
-
-    imageURL.forEach((result) => {
+    for (let i = 0; i < data.imageURL.length; i++) {
       let imageElement = document.createElement("img");
-      imageElement.setAttribute("src", `${result.webformatURL}`);
+      imageElement.setAttribute("src", `${data.imageURL[i].webformatURL}`);
       photoWrapper.appendChild(imageElement);
-    });
+    }
   } catch (error) {
-    console.log(error);
+    alert(error);
   }
 }
-
-// function getPhoto(keyword) {
-//   let apiURL = `/.netlify/functions/getPhotos?keyword=${keyword}`;
-
-//   fetch(apiURL, {
-//     method: "GET",
-//     headers: { accept: "application/json" },
-//   }).then((response) => response.json())
-//   // photoWrapper.innerHTML = JSON.stringify(response);
-
-//   .then((data) => {
-//     let imageURL = data.hits;
-//     // let imageURL = JSON.stringify(data.hits);
-//     // imageURL?.map((result) => {
-
-//     imageURL.forEach((result) => {
-//       let imageElement = document.createElement("img");
-//       imageElement.setAttribute("src", `${result.webformatURL}`);
-//       photoWrapper.appendChild(imageElement);
-//     });
-//   })
-//   .catch((error) => alert(error.message));
-// }
---
+++ netlify/functions/getPhotos.js
@@ -0,0 +1,27 @@
+require("dotenv").config();
+const axios = require("axios");
+
+exports.handler = async (event, context) => {
+  try {
+    const { keyword } = event.queryStringParameters;
+    let response = await axios.get(
+      `https://pixabay.com/api/?key=${process.env.PIXABAY_API_KEY}&q=${keyword}&image_type=photo&safesearch=true&per_page=3`,
+      {
+        headers: { Accept: "application/json", "Accept-Encoding": "identity" },
+        params: { trophies: true },
+      }
+    );
+
+    let imageURL = response.data.hits;
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ imageURL }),
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error }),
+    };
+  }
+};
