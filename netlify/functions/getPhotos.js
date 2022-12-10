078323802c24f75fd19cb85ce9b2c3c300d6ad2b
frankiefab100 <46662771+frankiefab100@users.noreply.github.com>
2022-12-10 20:29:40 +0100

    feat: add type attribute to script and import build config

--- netlify/functions/getPhotos.js
+++ netlify/functions/getPhotos.js
@@ -1,11 +1,16 @@
+import { PIXABAY_API_KEY } from "./config.js";
+
 require("dotenv").config();
 const axios = require("axios");
+
+// const process = require("process");
+// const { PIXABAY_API_KEY } = process.env;
 
 exports.handler = async (event, context) => {
   try {
     const { keyword } = event.queryStringParameters;
     let response = await axios.get(
-      `https://pixabay.com/api/?key=${process.env.PIXABAY_API_KEY}&q=${keyword}&image_type=photo&safesearch=true&per_page=3`,
+      `https://pixabay.com/api/?key=${PIXABAY_API_KEY}&q=${keyword}&image_type=photo&safesearch=true&per_page=3`,
       {
         headers: { Accept: "application/json", "Accept-Encoding": "identity" },
         params: { trophies: true },
