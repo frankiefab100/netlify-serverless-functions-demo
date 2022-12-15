6f358c3c5cecb8ca559a29583f7f446ab6284bbb
Franklin Ohaegbulam <46662771+frankiefab100@users.noreply.github.com>
2022-12-15 01:07:51 +0100

    Create script.js

---
+++ dist/script.js
@@ -0,0 +1,38 @@
+const searchbar = document.querySelector(".search");
+const submitBtn = document.querySelector(".search-btn");
+const photoWrapper = document.querySelector(".photo-wrapper");
+
+submitBtn.addEventListener("click", () => {
+  getPhoto(searchbar.value);
+  searchbar.value = "";
+});
+
+window.addEventListener("keydown", (e) => {
+  if (e.keyCode === 13) {
+    getPhoto(searchbar.value);
+    searchbar.value = "";
+  }
+});
+
+const dotenv = require("dotenv").config();
+
+function getPhoto(keyword) {
+  const apiKey = process.env.PIXABAY_API_KEY;
+  let apiURL = `https://pixabay.com/api/?key=${apiKey}&q=${keyword}&image_type=photo&safesearch=true&per_page=3`;
+
+  fetch(apiURL, {
+    method: "GET",
+    headers: { accept: "application/json" },
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      let imageURL = data.hits;
+
+      for (let i = 0; i < data.imageURL.length; i++) {
+      let imageElement = document.createElement("img");
+      imageElement.setAttribute("src", `${data.imageURL[i].webformatURL}`);
+      photoWrapper.appendChild(imageElement);
+      }
+    })
+    .catch((error) => alert(error.message));
+}
